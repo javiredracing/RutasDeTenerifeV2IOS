630737bc031ef9aa4209d9733a99c0e25085fb0e
Manuel Cendagorta G. López <iter@iter.es>
2016-02-04 16:39:32 +0000

    AppInfo revised

---
+++ RutasDeTenerife2/AppInfoTabViewController.h
@@ -0,0 +1,14 @@
+//
+//  AppInfoTabViewController.h
+//  RutasDeTenerife2
+//
+//  Created by javi on 4/2/16.
+//  Copyright © 2016 JAVI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppInfoTabViewController : UITabBarController
+- (IBAction)closeAppInfo:(UIBarButtonItem *)sender;
+
+@end
---
+++ RutasDeTenerife2/AppInfoTabViewController.m
@@ -0,0 +1,40 @@
+//
+//  AppInfoTabViewController.m
+//  RutasDeTenerife2
+//
+//  Created by javi on 4/2/16.
+//  Copyright © 2016 JAVI. All rights reserved.
+//
+
+#import "AppInfoTabViewController.h"
+
+@interface AppInfoTabViewController ()
+
+@end
+
+@implementation AppInfoTabViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)closeAppInfo:(UIBarButtonItem *)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+@end
